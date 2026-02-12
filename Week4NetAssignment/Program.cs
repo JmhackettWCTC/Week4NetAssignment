ed79448139333169e81637f0d43961529fd575d8
JmhackettWCTC <Jhackett2@my.wctc.edu>
2026-02-12 13:05:02 -0600

    csv file added & program structure created

--- Week4NetAssignment/Program.cs
+++ Week4NetAssignment/Program.cs
@@ -1,3 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 
-Console.WriteLine("Hello, World!");
+function menu()
+{
+    Console.WriteLine("-----------------------------");
+    Console.WriteLine("- Mario Bros Character Data -");
+    Console.WriteLine("-----------------------------");
+    
+    Console.WriteLine("1. View all characters");
+    Console.WriteLine("2. Add a character");
+    Console.WriteLine("3. Exit");
+    Console.Write("Please select an option: ");
+}
+
+
+function viewCharacters()
+{
+    Console.WriteLine("Viewing all characters...");
+    // Code to display characters goes here
+}
+
+function addCharacter()
+{
+    Console.WriteLine("Adding a new character...");
+    // Code to add a new character goes here
+}
+
+while (true)
+{
+    menu();
+    int answer;
+    if (!int.TryParse(Console.ReadLine(), out answer))
+    {
+        Console.WriteLine("Invalid input. Please enter a number.");
+        continue;
+    }
+
+    switch (answer)
+    {
+        case 1:
+            viewCharacters();
+            break;
+        case 2:
+            addCharacter();
+            break;
+        case 3:
+            Console.WriteLine("Exiting the program. Goodbye!");
+            return;
+        default:
+            Console.WriteLine("Invalid option. Please select 1, 2, or 3.");
+            break;
+    }
+}
