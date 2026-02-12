9fa0f4c6db5a0b31c9742e4ed8298f9f9f3a9dc5
JmhackettWCTC <Jhackett2@my.wctc.edu>
2026-02-12 13:13:16 -0600

    Program Structure Finsihed

--- Week4NetAssignment/Program.cs
+++ Week4NetAssignment/Program.cs
@@ -1,7 +1,35 @@
 ﻿
-
-function menu()
+while (true)
 {
+    Menu();
+    int answer;
+    if (!int.TryParse(Console.ReadLine(), out answer))
+    {
+        Console.WriteLine("Invalid input. Please enter a number.");
+        continue;
+    }
+
+    switch (answer)
+    {
+        case 1:
+            ViewCharacters();
+            break;
+        case 2:
+            AddCharacter();
+            break;
+        case 3:
+            Console.WriteLine("Exiting the program. Goodbye!");
+            return;
+        default:
+            Console.WriteLine("Invalid option. Please select 1, 2, or 3.");
+            break;
+    }
+}
+
+
+static void Menu()
+{
+    Console.Clear();
     Console.WriteLine("-----------------------------");
     Console.WriteLine("- Mario Bros Character Data -");
     Console.WriteLine("-----------------------------");
@@ -13,41 +41,21 @@ function menu()
 }
 
 
-function viewCharacters()
+static void ViewCharacters()
 {
+    Console.Clear();
     Console.WriteLine("Viewing all characters...");
     // Code to display characters goes here
+    Console.Write("press enter to leave");
+    Console.ReadLine();
 }
 
-function addCharacter()
+static void AddCharacter()
 {
+    Console.Clear();
     Console.WriteLine("Adding a new character...");
     // Code to add a new character goes here
-}
-
-while (true)
-{
-    menu();
-    int answer;
-    if (!int.TryParse(Console.ReadLine(), out answer))
-    {
-        Console.WriteLine("Invalid input. Please enter a number.");
-        continue;
-    }
-
-    switch (answer)
-    {
-        case 1:
-            viewCharacters();
-            break;
-        case 2:
-            addCharacter();
-            break;
-        case 3:
-            Console.WriteLine("Exiting the program. Goodbye!");
-            return;
-        default:
-            Console.WriteLine("Invalid option. Please select 1, 2, or 3.");
-            break;
-    }
+    Console.Write("press enter to leave");
+    Console.ReadLine();
+    
 }
