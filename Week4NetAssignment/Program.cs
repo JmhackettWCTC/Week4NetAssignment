2691604a95d4f5912709edcd7bf4e65211b99fd5
JmhackettWCTC <Jhackett2@my.wctc.edu>
2026-02-17 09:10:28 -0600

    attempt to use NLog

--- Week4NetAssignment/Program.cs
+++ Week4NetAssignment/Program.cs
@@ -1,30 +1,56 @@
-﻿
-while (true)
-{
-    Menu();
-    int answer;
-    if (!int.TryParse(Console.ReadLine(), out answer))
-    {
-        Console.WriteLine("Invalid input. Please enter a number.");
-        continue;
-    }
+﻿using NLog;
+String path = Directory.GetCurrentDirectory() + "//nlog.config";
 
-    switch (answer)
+// create instance of Logger
+var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
+
+logger.Info("Program started");
+
+String file = "mario.csv";
+// make sure movie file exists
+if (!File.Exists(file))
+{
+    logger.Error("File does not exist: {File}", file);
+    Console.WriteLine("File does not exist");
+}
+else
+{
+    
+
+    
+    
+    
+    while (true)
     {
-        case 1:
-            ViewCharacters();
-            break;
-        case 2:
-            AddCharacter();
-            break;
-        case 3:
-            Console.WriteLine("Exiting the program. Goodbye!");
-            return;
-        default:
-            Console.WriteLine("Invalid option. Please select 1, 2, or 3.");
-            break;
+        Menu();
+        int answer;
+        if (!int.TryParse(Console.ReadLine(), out answer))
+        {
+            Console.WriteLine("Invalid input. Please enter a number.");
+            Task.Delay(1000).Wait();
+            continue;
+        }
+
+        switch (answer)
+        {
+            case 1:
+                ViewCharacters();
+                break;
+            case 2:
+                AddCharacter();
+                break;
+            case 3:
+                Console.WriteLine("Exiting the program. Goodbye!");
+                Task.Delay(1000).Wait();
+                return;
+            default:
+                Console.WriteLine("Invalid option. Please select 1, 2, or 3.");
+                Task.Delay(1000).Wait();
+                break;
+        }
     }
 }
+
 
 
 static void Menu()
@@ -46,7 +72,10 @@ static void ViewCharacters()
     Console.Clear();
     Console.WriteLine("Viewing all characters...");
     // Code to display characters goes here
-    Console.Write("press enter to leave");
+    
+
+    
+    Console.Write("press any key to leave");
     Console.ReadLine();
 }
 
@@ -55,7 +84,9 @@ static void AddCharacter()
     Console.Clear();
     Console.WriteLine("Adding a new character...");
     // Code to add a new character goes here
-    Console.Write("press enter to leave");
+    Console.Write("press any key to leave");
     Console.ReadLine();
     
 }
+
+logger.Info("Program ended");
